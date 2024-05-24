c365fb62ec4c6141152d77bae8eb65f515093864
davidLeeeeeeeeeeee <lyg_work@foxmail.com>
2024-05-24 14:04:34 +0800

    Add files via upload

---
+++ filter_day_std_04.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from datetime import timedelta
+from tqdm import tqdm
+# 读取 CSV 文件
+df = pd.read_csv(r'filter_20group.csv')
+# 筛选出需要比较的合约字段
+# date_cols = [col for col in df.columns if 'timestamp' in col]
+date_cols = ['date_1','date_3','date_5','last_date' ]
+# 将日期时间列转换为 datetime 类型
+for col in date_cols:
+    df[col] = pd.to_datetime(df[col])
+
+# 按 type 分组
+grouped = df.groupby('Sybil_number')
+
+# 创建一个空的 DataFrame 用于存储过滤后的数据
+df_filtered = pd.DataFrame()
+
+# 遍历每个分组
+for type_value, group in tqdm(grouped):
+    is_insert = True
+    for date in date_cols:
+        # 计算当前组的 first_block_time 的标准差
+        std = group[date].diff().std()
+
+        if std > timedelta(days=3):
+            is_insert = False
+    if is_insert:
+        df_filtered = pd.concat([df_filtered, group.reset_index(drop=True)], axis=0)
+
+# 保存过滤后的结果为新的 CSV 文件
+df_filtered.to_csv(r'Sybil_Address135_day3.csv', index=False)
+
+print("Filtered data saved to 'Sybil_Address.csv'")
