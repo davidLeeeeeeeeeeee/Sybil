f18d10cbb544d2304a9862f1277730ba1b9f7d66
davidLeeeeeeeeeeee <lyg_work@foxmail.com>
2024-05-24 14:02:47 +0800

    Update filter_10_tx_count_01.py

--- filter_10_tx_count_01.py
+++ filter_10_tx_count_01.py
@@ -15,62 +15,88 @@ df_filtered = df[df['SENDER_WALLET'].isin(sender_wallet_count.index)]
 # 统计每个SENDER_WALLET的STARGATE_SWAP_USD的总和
 sender_wallet_sum = df_filtered.groupby('SENDER_WALLET')['STARGATE_SWAP_USD'].sum()
 
+# Convert 'SOURCE_TIMESTAMP_UTC' to datetime
+df_filtered['SOURCE_TIMESTAMP_UTC'] = pd.to_datetime(df_filtered['SOURCE_TIMESTAMP_UTC']).dt.date
+groupby_pd = df_filtered.groupby('SENDER_WALLET')
+# Group by 'SENDER_WALLET' and count unique 'SOURCE_TIMESTAMP_UTC'
+result_day = groupby_pd['SOURCE_TIMESTAMP_UTC'].nunique()
+
 # 初始化字典用于存储每个SENDER_WALLET的时间戳和目的地合约
 sender_wallet_timestamps = {}
 
 # 遍历每个SENDER_WALLET
-for wallet, group in tqdm(df_filtered.groupby('SENDER_WALLET')):
-    # 按照时间戳排序
+for wallet, group in tqdm(groupby_pd):
+    # 按照时间戳排序 SOURCE_CONTRACT
     sorted_group = group.sort_values(by='SOURCE_TIMESTAMP_UTC')
+    timestamps = sorted_group['SOURCE_TIMESTAMP_UTC'].tolist()
+    top_contracts = sorted_group['DESTINATION_CONTRACT'].tolist()
+    top_source = sorted_group['SOURCE_CONTRACT'].tolist()
     sender_wallet_timestamps[wallet] = {
-        'timestamp': sorted_group['SOURCE_TIMESTAMP_UTC'].tolist(),
-        'contract': sorted_group['DESTINATION_CONTRACT'].tolist()
+        'Activate_Date': timestamps[0],
+        'date_1': timestamps[0],
+        'date_2': timestamps[1],
+        'date_3': timestamps[2],
+        'date_4': timestamps[3],
+        'date_5': timestamps[4],
+        'date_6': timestamps[5],
+        'date_7': timestamps[6],
+        'date_8': timestamps[7],
+        'date_9': timestamps[8],
+        'date_10': timestamps[9],
+        'last_date': timestamps[-1],
+        'last_date_2': timestamps[-2],
+        'action_1': top_contracts[0]+ '+' + top_source[0],
+        'action_2': top_contracts[1]+ '+' + top_source[1],
+        'action_3': top_contracts[2]+ '+' + top_source[2],
+        'action_4': top_contracts[3]+ '+' + top_source[3],
+        'action_5': top_contracts[4]+ '+' + top_source[4],
+        'action_6': top_contracts[5]+ '+' + top_source[5],
+        'action_7': top_contracts[6]+ '+' + top_source[6],
+        'action_8': top_contracts[7]+ '+' + top_source[7],
+        'action_9': top_contracts[8]+ '+' + top_source[8],
+        'action_10': top_contracts[9]+ '+' + top_source[9],
+        'last_contract': top_contracts[-1]+ '+' + top_source[-1],
+        'last_contract_2': top_contracts[-2]+ '+' + top_source[-2],
     }
 
 # 计算每个SENDER_WALLET的STARGATE_SWAP_USD的平均值
 avg_swap_usd = sender_wallet_sum / sender_wallet_count
 
-# 构建结果DataFrame
-results = pd.DataFrame({
-    'SENDER_WALLET': sender_wallet_count.index,
-    'tx_count': sender_wallet_count.values,
-    'avg_swap_usd': avg_swap_usd.values
-})
+# 将每个 Series 转换为 DataFrame，并指定列名
+sender_wallet_count_df = pd.DataFrame({'SENDER_WALLET': sender_wallet_count.index, 'tx_count': sender_wallet_count.values}, index=sender_wallet_count.index)
+avg_swap_usd_df = pd.DataFrame({'avg_swap_usd': avg_swap_usd.values}, index=avg_swap_usd.index)
+result_day_df = pd.DataFrame({'LZ_Age_In_Days': result_day.values}, index=result_day.index)
 
-# 获取每个SENDER_WALLET的前10笔和最后2笔交易的时间戳和合约
-first_ten_timestamps = []
-last_two_timestamps = []
-first_ten_contracts = []
-last_two_contracts = []
+# 使用 concat() 函数将它们按列合并为一个 DataFrame
+results = pd.concat([sender_wallet_count_df, avg_swap_usd_df, result_day_df], axis=1)
 
-for wallet in tqdm(sender_wallet_timestamps, desc="Processing transactions"):
-    # 获取前10笔交易时间戳和合约
-    first_ten_timestamps.append(sender_wallet_timestamps[wallet]['timestamp'][:10])
-    first_ten_contracts.append(sender_wallet_timestamps[wallet]['contract'][:10])
-
-    # 获取最后2笔交易时间戳和合约
-    last_two_timestamps.append(sender_wallet_timestamps[wallet]['timestamp'][-2:])
-    last_two_contracts.append(sender_wallet_timestamps[wallet]['contract'][-2:])
-
-# 将前10笔和最后2笔交易的时间戳和合约作为单独的属性列表
-for i in range(10):
-    results[f'first_timestamp_{i + 1}'] = [timestamps[i] if len(timestamps) > i else None for timestamps in
-                                           first_ten_timestamps]
-
-for i in range(2):
-    results[f'last_timestamp_{i + 1}'] = [timestamps[i] if len(timestamps) > i else None for timestamps in
-                                          last_two_timestamps]
-
-for i in range(10):
-    results[f'first_contract_{i + 1}'] = [contracts[i] if len(contracts) > i else None for contracts in
-                                          first_ten_contracts]
-
-for i in range(2):
-    results[f'last_contract_{i + 1}'] = [contracts[i] if len(contracts) > i else None for contracts in
-                                         last_two_contracts]
-
+# 使用 map 函数将字典中的值映射到新列
+results['Activate_Date'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['Activate_Date'])
+results['date_1'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_1'])
+results['date_2'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_2'])
+results['date_3'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_3'])
+results['date_4'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_4'])
+results['date_5'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_5'])
+results['date_6'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_6'])
+results['date_7'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_7'])
+results['date_8'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_8'])
+results['date_9'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_9'])
+results['date_10'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['date_10'])
+results['last_date'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['last_date'])
+results['last_date_2'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['last_date_2'])
+results['action_1'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_1'])
+results['action_2'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_2'])
+results['action_3'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_3'])
+results['action_4'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_4'])
+results['action_5'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_5'])
+results['action_6'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_6'])
+results['action_7'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_7'])
+results['action_8'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_8'])
+results['action_9'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_9'])
+results['action_10'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['action_10'])
+results['last_contract'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['last_contract'])
+results['last_contract_2'] = results['SENDER_WALLET'].map(lambda x: sender_wallet_timestamps[x]['last_contract_2'])
 # 保存结果到CSV文件
 results.to_csv('10tx_count.csv', index=False)
-
 
 
